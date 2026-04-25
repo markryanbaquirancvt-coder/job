138dccbd65e2250561141ca48d1d652d20022b83
markryanbaquirancvt-coder <markryan.baquiran.cvt@eac.edu.ph>
2026-04-25 22:16:30 +0800

    Create dbConfig.php

---
+++ core/dbConfig.php
@@ -0,0 +1,17 @@
+<?php
+$host = "127.0.0.1";
+$port = "3307"; 
+$user = "root";
+$password = ""; 
+$dbname = "dream_job_db";
+
+$dsn = "mysql:host={$host};port={$port};dbname={$dbname};charset=utf8mb4";
+
+try {
+    $pdo = new PDO($dsn, $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
